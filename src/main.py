aa214f8091213bafd1ee19b64b9450488e2bc0cb
Thomas Chapeaux <chapeauxthomas@gmail.com>
2014-03-16 23:05:04 +0100

    command-line choices!

--- src/main.py
+++ src/main.py
@@ -3,26 +3,31 @@ from direction import DIRECTIONS
 from game import Game
 
 
+print("1) Launch AI")
+print("2) Play yourself")
+choice = input("Your choice? [1-2] ")
+
 g = Game()
-ai = ThreesAI(game=g)
-ai.loop()
-
-exit(0)
-
-while True:
-    print("--------------")
-    print(g.grid)
-    print("--------------")
-    print("Current Score:", g.grid.score())
-    validInput = False
-    while not validInput:
-        print("Next Piece:", g.nextTile.value)
-        userDirection = input("Direction? (up, down, left, right) ")
-        try:
-            if userDirection == "quit":
-                exit(0)
-            userDirection = DIRECTIONS[userDirection.lower()]
-            validInput = True
-        except KeyError as e:
-            print("Invalid direction (" + str(userDirection) + ")")
-    g.userInput(userDirection)
+if choice == "1":
+    ai = ThreesAI(game=g)
+    ai.loop()
+elif choice == "2":
+    while True:
+        print("--------------")
+        print(g.grid)
+        print("--------------")
+        print("Current Score:", g.grid.score())
+        validInput = False
+        while not validInput:
+            print("Next Piece:", g.nextTile.value)
+            userDirection = input("Direction? (up, down, left, right) ")
+            try:
+                if userDirection == "quit":
+                    exit(0)
+                userDirection = DIRECTIONS[userDirection.lower()]
+                validInput = True
+            except KeyError as e:
+                print("Invalid direction (" + str(userDirection) + ")")
+        g.userInput(userDirection)
+else:
+    print("Did not get that. Exiting.")
